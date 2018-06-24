809d4a39298c30047a68586568c57f22c83b4e98
zero <zeuslfhj@gmail.com>
2018-06-24 23:10:13 +0800

    fix display error when add panel to html, add build scripts to package.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Toast = {
         const ele = doc.createElement('div');
         ele.innerHTML = `
             <div class="toast-container"
-                style="position: fixed; width: 100%; height: 100%; background-color: rgba(0,0,0,0.5); display: none">
+                style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.5); display: none">
                 <div class="toast-content-container"
                     style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); -webkit-transform: translate(-50%, -50%);"
                 >
